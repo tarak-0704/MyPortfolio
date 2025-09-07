526023259e4935be3e672de9b2cb30656448ebc9
Taraka Manikanta Nallana <tarakamanikanta8@gmail.com>
2025-09-07 19:06:48 +0530

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './sections/hero/hero.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { EducationComponent } from './sections/education/education.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    EducationComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
